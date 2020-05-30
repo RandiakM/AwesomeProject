915d1cd34cc8894c0a7b312c54db2589b86d39c8
RandiakM <randikamadu1995@gmail.com>
2020-05-31 02:21:00 +0530

    Init Upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Hello! React Native.</Text>
+      <Image source={require("./assets/icon.png")} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
