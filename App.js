435bced0dcc3ecadffe006f20ad46ab73d6ab3ce
RandiakM <randikamadu1995@gmail.com>
2020-05-31 13:33:51 +0530

    text, button, statusbar check

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import React from "react";
-import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Alert,
+  Button,
+  StatusBar,
+  Platform,
+} from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello! React Native.</Text>
-      <Image source={require("./assets/icon.png")} />
+      <Button
+        color="lightgreen"
+        title="Click Me"
+        onPress={() => console.log("Button Tapped")}
+      ></Button>
+      <Button
+        title="Click Me button 2"
+        onPress={() =>
+          Alert.alert("My title", "My message", [
+            { text: "Yes", onPress: () => console.log("Yes") },
+            { text: "No", onPress: () => console.log("No") },
+          ])
+        }
+      ></Button>
+
+      <Button
+        color="orange"
+        title="Click Me button 3"
+        onPress={() =>
+          Alert.prompt("My title", "My message", (text) => console.log(text))
+        }
+      ></Button>
     </SafeAreaView>
   );
 }
@@ -14,7 +44,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
