0b4a0d53a516954da0e1e7f0e4a440a0a0bb69e8
RandiakM <randikamadu1995@gmail.com>
2020-07-05 00:07:38 +0530

    Absolute and relative positioning

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ export default function App() {
         style={{
           backgroundColor: "dodgerblue",
           width: 100,
-          height: 300,
+          height: 100,
         }}
       />
       <View
@@ -26,25 +26,14 @@ export default function App() {
           backgroundColor: "gold",
           width: 100,
           height: 100,
+          top: 20,
+          left: 20,
+          position: "relative",
         }}
       />
       <View
         style={{
           backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "grey",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "greenyellow",
           width: 100,
           height: 100,
         }}
